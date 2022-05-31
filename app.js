9718797fb4ac5905c4a6c047f8fc931bf6f07b28
Hamzaansari <hamzaansaree15@gmail.com>
2022-05-31 11:41:28 -0700

    CHAPTER # 3 COMPLETED

---
+++ app.js
@@ -0,0 +1,8 @@
+var age = "i am 17 years old";
+alert(age);
+var userVisiting = "you are visited 12 times in this website";
+alert(userVisiting);
+var birthYear = "My birth year is 2004";
+document.write(birthYear);
+var clientOrder = "John Doe order 5 T-shirts on xyz store";
+document.write(clientOrder); 
